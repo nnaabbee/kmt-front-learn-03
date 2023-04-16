9713f01d355793e749d9e7b792295e689e799081
鍋島 章祐 <job.nnaabbee.programming0106@gmail.com>
2023-04-16 03:41:57 +0000

    勉強会でやったところまでコミット TodoのCreateおよび、Get

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+type Todo = {
+  id: number
+  title: string
+  content: string
+  isDone: boolean
 }
+
+const inMemoryTodos: Todo[] = [
+  { id: 1, title: "初期投入データタイトル", content: "初期投入データコンテンツ", isDone: false }
+]
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Todo[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "GET") {
+    res.status(200).json(inMemoryTodos)
+    return
+  }
+  if (req.method === "POST") {
+    const newTodo = { ...req.body, id: inMemoryTodos.length + 1 }
+    inMemoryTodos.push(newTodo)
+    res.status(200).json(inMemoryTodos)
+    return
+  }
+  res.status(200).json(inMemoryTodos)
 }
--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -1,13 +1,83 @@
 import Head from "next/head"
-import { Label } from "@/components/Label"
+import Link from "next/link"
+import { ChangeEvent, useState } from "react"
+import axios from "axios"
+import { Button } from "@/components/Button"
+
+// APIから戻ってくるTodoの型
+type Todo = {
+  id: number
+  title: string
+  content: string
+  isDone: boolean
+}
+
+// フロント側でTodoを作成するときの型
+type PostTodo = Omit<Todo, "id">
 
 export default function Home() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  const onChangeContent = (event: ChangeEvent<HTMLInputElement>) => {
+    setContent(event.target.value)
+  }
+
+  const fetchTodos = () => {
+    axios.get("/api/hello").then((res) => {
+      setTodos(res.data)
+    })
+  }
+
+  const registerTodo = () => {
+    const newTodo: PostTodo = {
+      title,
+      content,
+      isDone: false,
+    }
+    axios.post("/api/hello", newTodo).then((res) => {
+      setTodos(res.data)
+    })
+  }
+
   return (
     <>
       <Head>
         <title>Todos</title>
       </Head>
       <h1>Todos</h1>
+      <div>
+        <p>タイトル：</p>
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <p>コンテンツ：</p>
+        <input value={content} onChange={onChangeContent} />
+      </div>
+      <Button buttonName="Todo登録" onClick={registerTodo} />
+      <Button buttonName="Todo取得" onClick={fetchTodos} />
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <span>{todo.id}</span>
+            <span> - </span>
+            <span>{todo.title}</span>
+            <span> - </span>
+            <span>{todo.content}</span>
+            <span> - </span>
+            <span>{todo.isDone ? "済" : "未"}</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <Link href="/">Topページへ戻る</Link>
+      </div>
     </>
   )
 }
